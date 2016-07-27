f37171aca583cd49b3219cb62eb385a6d8d2f5d1
joedaouddotcom <joedaoud@outlook.com>
2016-07-27 08:35:50 -0700

    Configure Mail Service and MVC Settings

--- src/jddc-TheWorld-2.0/Controllers/Web/AppController.cs
+++ src/jddc-TheWorld-2.0/Controllers/Web/AppController.cs
@@ -5,11 +5,23 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using jddc_TheWorld_2._0.ViewModels;
+using jddc_TheWorld_2._0.Services;
+using Microsoft.Extensions.Configuration;
 
 namespace jddc_TheWorld_2._0.Controllers.Web
 {
     public class AppController : Controller
     {
+        private IMailService _mailService;
+        private IConfigurationRoot _config;
+
+        public AppController(IMailService mailService, IConfigurationRoot config)
+        {
+            _mailService = mailService;
+            _config = config;
+        }
+
+
         public IActionResult Index()
         {
             return View();
@@ -23,6 +35,20 @@ namespace jddc_TheWorld_2._0.Controllers.Web
         [HttpPost]
         public IActionResult Contact(ContactViewModel model)
         {
+            if (model.Email.Contains("aol.com"))
+            {
+                ModelState.AddModelError("Email", "We don't support aol.com addresses");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _mailService.SendMail(_config["MailSettings:ToAddress"], model.Email, "From jddc-TheWorld-2.0", model.Message);
+
+                ModelState.Clear();
+
+                ViewBag.UserMessage = "Message Sent!";
+            }
+
             return View();
         }
 
