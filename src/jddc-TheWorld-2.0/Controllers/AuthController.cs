846f99b10ff387b3727609dd840041e8b12c796b
joedaouddotcom <joedaoud@outlook.com>
2016-07-28 16:25:21 -0700

    Design Login View.

---
+++ src/jddc-TheWorld-2.0/Controllers/AuthController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jddc_TheWorld_2._0.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login()
+        {
+            if(User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Trips", "App");
+            }
+
+            return View();
+        }
+    }
+}
