72d3e1e50ea9388c0a2a27da93575413d81cbbfa
joedaouddotcom <joedaoud@outlook.com>
2016-07-28 08:49:43 -0700

    Add Seed Data.

--- src/jddc-TheWorld-2.0/Startup.cs
+++ src/jddc-TheWorld-2.0/Startup.cs
@@ -47,12 +47,14 @@ namespace jddc_TheWorld_2._0
 
             services.AddDbContext<WorldContext>();
 
+            services.AddTransient<WorldContextSeedData>();
+
             services.AddMvc();
 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, WorldContextSeedData seeder)
         {
             if (env.IsEnvironment("Development"))
             {
@@ -70,6 +72,8 @@ namespace jddc_TheWorld_2._0
                     defaults: new { controller = "App", action = "Index" }
                     );
             });
+
+            seeder.EnsureSeedData().Wait();
         }
     }
 }
