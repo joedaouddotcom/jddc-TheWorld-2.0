56ad7162e63bd803a80e9593cf60e26b5dbe1fbc
joedaouddotcom <joedaoud@outlook.com>
2016-07-29 16:40:53 -0700

    Retreiving trips from database.

--- src/jddc-TheWorld-2.0/wwwroot/js/tripsController.js
+++ src/jddc-TheWorld-2.0/wwwroot/js/tripsController.js
@@ -7,7 +7,7 @@
     angular.module("app-trips")
     .controller("tripsController", tripsController);
 
-    function tripsController() {
+    function tripsController($http) {
         
         var vm = this;
 
@@ -20,6 +20,22 @@
         }];
 
         vm.newTrip = {};
+
+        vm.errorMessage = "";
+        vm.isBusy = true;
+
+        $http.get("/api/trips")
+        .then(function (response) {
+            //Success
+            angular.copy(response.data, vm.trips);
+
+        }, function (error) {
+            //Failure
+            vm.errorMessage = "Failed to load data: " + error;
+        })
+        .finally(function () {
+            vm.isBusy = false;
+        });
 
         vm.addTrip = function () {
             vm.trips.push({ name: vm.newTrip.name, created: new Date() });
